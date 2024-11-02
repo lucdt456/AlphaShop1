e095256d4060c9cfeb90bb4ed8711acfb3deca8d
lucdt456 <lucdt456@gmail.com>
2024-11-02 23:52:27 +0700

    Chỉnh sửa phân trang + tùy chọn số lượng sản phẩm hiển thị

--- AlphaShop1/Controllers/ShopController.cs
+++ AlphaShop1/Controllers/ShopController.cs
@@ -17,11 +17,9 @@ namespace AlphaShop1.Controllers
 		}
 
 		//Hiển thị danh sách hàng hóa + Tìm kiếm
-		public async Task<IActionResult> Index(string query = "",int pg = 1)
+		public async Task<IActionResult> Index(string query = "",int pg = 1, int size = 8)
 		{
 			List<ProductModel> products = await _dataContext.Products.OrderByDescending(p => p.Id).Where(p => p.Name.Contains(query)).Include(p => p.Category).Include(p => p.Brand).ToListAsync();
-
-			const int pagSize = 3;
 
 			if (pg < 1)
 			{
@@ -29,12 +27,13 @@ namespace AlphaShop1.Controllers
 			}
 
 			int recsCount = products.Count();
-			var pager = new Paginate(recsCount, pg, pagSize);
-			int recSkip = (pg - 1) * pagSize;
+			var pager = new Paginate(recsCount, pg, size);
+			int recSkip = (pg - 1) * size;
 
 			var data = products.Skip(recSkip).Take(pager.PageSize).ToList();
 			ViewBag.Pager = pager;
 			ViewBag.Query = query;
+			ViewBag.Size = size;
 			return View(data);
 		}
 	}
